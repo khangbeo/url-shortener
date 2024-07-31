eab102c1ae51520c8f0a831b487e40e5a5e70eac
Khang <khangbeo2012@gmail.com>
2024-07-30 18:41:15 -0700

    add clipboard icon to copy url button

--- app/history.js
+++ app/history.js
@@ -1,9 +1,17 @@
 import Button from "./components/Button";
 import Link from "next/link";
 import { FaClipboardList, FaRegTrashAlt } from "react-icons/fa";
-import { TiDelete } from "react-icons/ti";
+import { useState } from "react";
 
-export default function History({ pastUrls, setPastUrls, loading, removeUrl }) {
+export default function History({
+    pastUrls,
+    setPastUrls,
+    loading,
+    removeUrl,
+    isCopied,
+    currentCopiedUrl,
+    handleCopy,
+}) {
     const clearHistory = () => {
         setPastUrls([]);
     };
@@ -14,15 +22,15 @@ export default function History({ pastUrls, setPastUrls, loading, removeUrl }) {
                 <div className="card card-bordered bg-base-200 text-base-content w-full">
                     <div className="card-body items-center text-center">
                         <h3 className="card-title text-2xl">History</h3>
-                        <ul className="card-body">
+                        <ul>
                             {pastUrls.map((url) => (
                                 <li
                                     key={url.id}
                                     className="mt-3 flex items-center justify-between"
                                 >
-                                    <span>
+                                    {/* <span>
                                         {url.date} {url.time}
-                                    </span>
+                                    </span> */}
                                     <div className="bg-base-300 p-4 mx-3 rounded-2xl">
                                         <Link
                                             className="text-blue-500 hover:text-blue-700"
@@ -32,40 +40,47 @@ export default function History({ pastUrls, setPastUrls, loading, removeUrl }) {
                                             {url.newUrl}
                                         </Link>
 
-                                        <Button
-                                            loading={loading}
-                                            extra="ml-3 btn-square btn-ghost"
-                                            onClick={() =>
-                                                navigator.clipboard.writeText(
-                                                    url.newUrl
-                                                )
-                                            }
+                                        <span
+                                            className={`${
+                                                isCopied &&
+                                                currentCopiedUrl ===
+                                                    url.newUrl &&
+                                                "tooltip tooltip-open tooltip-top transition-all"
+                                            }`}
+                                            data-tip="copied"
                                         >
-                                            <FaClipboardList size={20} />
+                                            <Button
+                                                loading={loading}
+                                                extra="ml-3 btn-square btn-ghost"
+                                                onClick={() =>
+                                                    handleCopy(url.newUrl)
+                                                }
+                                            >
+                                                <FaClipboardList size={20} />
+                                            </Button>
+                                        </span>
+                                        <Button
+                                            type="error"
+                                            loading={loading}
+                                            extra="ml-3 btn-square btn-outline"
+                                            onClick={() => removeUrl(url.id)}
+                                        >
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                className="h-6 w-6"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                stroke="currentColor"
+                                            >
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    strokeWidth="2"
+                                                    d="M6 18L18 6M6 6l12 12"
+                                                />
+                                            </svg>
                                         </Button>
                                     </div>
-
-                                    <Button
-                                        type="error"
-                                        loading={loading}
-                                        extra="ml-3 btn-square btn-outline"
-                                        onClick={() => removeUrl(url.id)}
-                                    >
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            className="h-6 w-6"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            stroke="currentColor"
-                                        >
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="2"
-                                                d="M6 18L18 6M6 6l12 12"
-                                            />
-                                        </svg>
-                                    </Button>
                                 </li>
                             ))}
                         </ul>
--- app/page.js
+++ app/page.js
@@ -5,6 +5,8 @@ import useLocalStorage from "./hooks/useLocalStorage";
 import Button from "./components/Button";
 import History from "./history";
 import { LuShrink } from "react-icons/lu";
+import { FaClipboardList } from "react-icons/fa";
+import ThemeToggle from "./components/ThemeToggle";
 
 export default function Home() {
     const [url, setUrl] = useState("");
@@ -13,19 +15,15 @@ export default function Home() {
     const [theme, setTheme] = useLocalStorage("theme", "light");
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
-
-    function handleError(error) {
-        if (error) setError(error);
-        setTimeout(() => {
-            setError(null);
-        }, 5000);
-    }
+    const [isCopied, setIsCopied] = useState(false);
+    const [currentCopiedUrl, setCurrentCopiedUrl] = useState(null);
 
     function isInHistory(newUrl) {
         return pastUrls.some((url) => url.oldUrl === newUrl.oldUrl);
     }
 
     const shortenUrl = async (url) => {
+        setLoading(true);
         const baseUrl = `/api/shortener`;
         const options = {
             method: "POST",
@@ -46,6 +44,8 @@ export default function Home() {
             }
         } catch (error) {
             handleError(error.message);
+        } finally {
+            setLoading(false);
         }
     };
 
@@ -84,53 +84,42 @@ export default function Home() {
             return;
         }
 
-        setLoading(true);
         try {
             shortenUrl(url);
             setShortenedUrl("");
             setUrl("");
         } catch (error) {
             handleError(error.message);
-        } finally {
-            setLoading(false);
         }
     };
 
-    console.log(loading);
-    const handleChange = ({ target: { value } }) => setUrl(value);
+    const handleInputChange = ({ target: { value } }) => setUrl(value);
     const handleThemeChange = () => {
         setTheme(theme === "light" ? "dark" : "light");
+    };
+
+    function handleError(error) {
+        if (error) setError(error);
+        setTimeout(() => {
+            setError(null);
+        }, 5000);
+    }
+
+    const handleCopy = (url) => {
+        navigator.clipboard.writeText(url);
+        setCurrentCopiedUrl(url);
+        setIsCopied(true);
+        setTimeout(() => {
+            setIsCopied(false);
+        }, 2000);
     };
     return (
         <div className="flex flex-col min-h-screen" data-theme={theme}>
             <main className="flex-grow flex flex-col items-center justify-between p-20">
-                <label className="swap swap-rotate fixed right-4 top-4 z-50">
-                    {/* this hidden checkbox controls the state */}
-                    <input
-                        type="checkbox"
-                        className="theme-controller"
-                        checked={theme === "light" ? true : false}
-                        onChange={handleThemeChange}
-                    />
-
-                    {/* sun icon */}
-                    <svg
-                        className="swap-off h-10 w-10 fill-current"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                    >
-                        <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-                    </svg>
-
-                    {/* moon icon */}
-                    <svg
-                        className="swap-on h-10 w-10 fill-current"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                    >
-                        <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-                    </svg>
-                </label>
+                <ThemeToggle
+                    theme={theme}
+                    handleThemeChange={handleThemeChange}
+                />
 
                 <div className="flex flex-col m-auto z-10 w-full max-w-5xl items-center justify-center gap-12">
                     <h1 className="text-center text-6xl font-bold">
@@ -163,7 +152,7 @@ export default function Home() {
                             <input
                                 className="rounded-l-full input input-bordered join-item"
                                 id="url"
-                                onChange={handleChange}
+                                onChange={handleInputChange}
                                 type="text"
                                 name="url"
                                 placeholder="http://google.com"
@@ -175,11 +164,12 @@ export default function Home() {
                                 extra="join-item rounded-r-full"
                             >
                                 <LuShrink size={24} />
-                                Shorten
                             </Button>
                         </div>
                     </form>
-                    {loading && <div>loading...</div>}
+                    {loading && (
+                        <span className="loading loading-spinner loading-xs size-10"></span>
+                    )}
                     {shortenedUrl && (
                         <div className="card bg-base-200 text-base-content w-full md:w-4/5 lg:w-6/12">
                             <div className="card-body items-center text-center">
@@ -194,18 +184,24 @@ export default function Home() {
                                     >
                                         {shortenedUrl}
                                     </a>
-                                    <Button
-                                        type="primary"
-                                        loading={loading}
-                                        extra="mx-2"
-                                        onClick={() =>
-                                            navigator.clipboard.writeText(
-                                                shortenedUrl
-                                            )
-                                        }
+                                    <span
+                                        className={`${
+                                            isCopied &&
+                                            currentCopiedUrl === url.newUrl &&
+                                            "tooltip tooltip-open tooltip-top transition-all"
+                                        }`}
+                                        data-tip="copied"
                                     >
-                                        Copy
-                                    </Button>
+                                        <Button
+                                            loading={loading}
+                                            extra="ml-3 btn-square btn-ghost"
+                                            onClick={() =>
+                                                handleCopy(url.newUrl)
+                                            }
+                                        >
+                                            <FaClipboardList size={20} />
+                                        </Button>
+                                    </span>
                                 </div>
                             </div>
                         </div>
@@ -213,9 +209,12 @@ export default function Home() {
 
                     <History
                         pastUrls={pastUrls}
-                        removeUrl={removeUrl}
-                        loading={loading}
                         setPastUrls={setPastUrls}
+                        loading={loading}
+                        removeUrl={removeUrl}
+                        isCopied={isCopied}
+                        handleCopy={handleCopy}
+                        currentCopiedUrl={currentCopiedUrl}
                     />
                 </div>
             </main>
